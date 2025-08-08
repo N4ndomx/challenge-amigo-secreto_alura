03d7674055dc1b7cd601bdaeb886436580cd9127
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2025-08-08 15:41:03 -0600

    agregardo logica del juego

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const ID_UL_AMIGOS = 'listaAmigos'
+const ID_INPUT ='amigo'
+const ID_RESULTADO = 'resultado';
 
 
+let amigos = [];
+function sortearAmigo() {
+    const amigo = sortearAmigosArray()
+    if (amigo) {
+        asignarTextoElemento(ID_RESULTADO,amigo)
+        limpiarDatos(ID_UL_AMIGOS);
+        amigos=[]
+    }else{
+        alert('No haya amigos disponibles para sortear')
+    }
+}
+
+function agregarAmigo() {
+    limpiarDatos(ID_UL_AMIGOS)
+    limpiarDatos(ID_RESULTADO)
+    let nombre_amigo = document.getElementById(ID_INPUT).value;
+    if (esNombreValido(nombre_amigo)) {
+        amigos.push(nombre_amigo);
+        mostrarListaAmigos()
+        limpiarInput(ID_INPUT)
+    }else{
+        alert("Por favor, inserte un nombre");
+    }
+    
+}
+
+function sortearAmigosArray(){
+    if (amigos.length === 0) return null;
+    const amigoSeleccionado = amigos[ Math.floor(Math.random() * amigos.length)];
+    return amigoSeleccionado;
+}
+
+function agregarAmigoALista(nombre) {
+    let ul = document.getElementById(ID_UL_AMIGOS);
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(nombre));
+    ul.appendChild(li);
+}
+function limpiarDatos(id) {
+    let lista = document.getElementById(id);
+    lista.innerHTML = ""
+}
+function mostrarListaAmigos(){
+    limpiarDatos(ID_UL_AMIGOS)
+    amigos.forEach((value =>{
+        agregarAmigoALista(value)
+    }))
+}
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+function limpiarInput(id) {
+    document.getElementById(id).value = '';
+}
+function esNombreValido(nombre) {
+    return nombre && nombre !== '';
+}
